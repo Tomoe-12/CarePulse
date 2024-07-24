05fc65ed1c59dd9f2471fb7185840ac1cf3857ba
Tomoe-12 <li8993han@gmail.com>
2024-07-24 21:09:56 +0630

    redirected admin page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PassKeyModal from "@/components/PassKeyModal";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({searchParams} : SearchParamProps) {
+  const isAdmin = searchParams.admin == 'true'
   return (
     <div className="flex h-screen max-h-screen">
 
-{/* Todo : OTP verfication  | PasskeyModel*/}
+{isAdmin && <PassKeyModal/>}
 
 
       <section className="remove-scrollbar container my-auto">
