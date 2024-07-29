7cb1fd599ebbbf9bc7c0831b220ded5dfa921565
Tomoe-12 <li8993han@gmail.com>
2024-07-29 19:58:07 +0630

    adding sentry

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -4,12 +4,14 @@ import { Button } from "@/components/ui/button";
 import { getPatient } from "@/lib/actions/patient.actions";
 import Image from "next/image";
 import Link from "next/link";
+import * as Sentry from '@sentry/nextjs'
 
 export default async function NewAppointment({
   params: { userId },
 }: SearchParamProps) {
 
     const patient = await getPatient(userId)
+    Sentry.metrics.set("user_view_new-appointment", patient.name);
     
 
   return (
--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -5,6 +5,8 @@ import { formatDateTime } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import * as Sentry from '@sentry/nextjs'
+import { getUser } from "@/lib/actions/patient.actions";
 
 // http://localhost:3000/patients/6698672100165c9ea9c3/new-appointment/success?appointmentId=669fb703001882a5ead8
 const Success = async ({
@@ -17,8 +19,9 @@ const Success = async ({
   const doctor = Doctors.find(
     (doc) => doc.name === appointment.primaryPhysician
   );
-  console.log("appointment : ", appointment);
-  console.log("doctor  ;", doctor);
+  const user = await getUser(userId);
+
+  Sentry.metrics.set("user_view_appointment-success", user.name);
 
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
